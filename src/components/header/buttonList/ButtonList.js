771b6c9fe01261f0eec7fdde6e4464e266507e72
codersudhir <nandanesudhir1@gmail.com>
2023-02-06 22:46:45 +0530

    fd

--- src/components/header/buttonList/ButtonList.js
+++ src/components/header/buttonList/ButtonList.js
@@ -6,20 +6,20 @@ import { Link } from 'react-router-dom';
 const ButtonList = () => {
  
   return (
-    <div className="button_list" style={{position:"fixed"}} >
+    <div className="button_list" style={{position:"fixed"}} id="nav-menu" >
       <HStack marginLeft={"80%"} mt="2" mr="4" className="button_stack"   >
         <Link to="/">
-          <button>Home</button>
+          <button  class="nav-link home">Home</button>
         </Link>
         <Link to="/skills">
-          <button>Skills</button>
+          <button  class="nav-link skills">Skills</button>
         </Link>
         <Link to="/projects">
-          <button>Projects</button>
+          <button class="nav-link projects">Projects</button>
         </Link>
-        <a href="https://docs.google.com/document/d/1_bWH28nOAnBCozL8CrT66WpHO_9jXLN_xWea7NnGeBc/edit" target="_blank" rel='noreferrer'><button>Resume</button></a>
+        <a href="https://docs.google.com/document/d/1_bWH28nOAnBCozL8CrT66WpHO_9jXLN_xWea7NnGeBc/edit" target="_blank" rel='noreferrer'><button class="nav-link resume">Resume</button></a>
         <Link to="/contact">
-          <button>Contact</button>
+          <button class="nav-link contact">Contact</button>
         </Link>
       </HStack>
     </div>
