667a742661ee9d759cb2a3c5ac5bae5d59fb6c23
codersudhir <nandanesudhir1@gmail.com>
2023-02-15 10:34:23 +0530

    gdg

--- src/components/header/buttonList/ButtonList.jsx
+++ src/components/header/buttonList/ButtonList.jsx
@@ -6,7 +6,8 @@ import { Link } from 'react-router-dom';
 const ButtonList = () => {
  
   return (
-    <div className="button_list" style={{position:"fixed"}} id="nav-menu"  >
+    <div id="nav-menu" >
+          <div className="button_list" style={{position:"fixed"}}  >
       <HStack marginLeft={"80%"} mt="2" mr="4" className="button_stack"    >
         <Link to="/" className="nav-link home">
           <button  >Home</button>
@@ -17,12 +18,15 @@ const ButtonList = () => {
         <Link to="/projects" className="nav-link projects">
           <button >Projects</button>
         </Link>
-        <a href="https://docs.google.com/document/d/1_bWH28nOAnBCozL8CrT66WpHO_9jXLN_xWea7NnGeBc/edit" target="_blank" rel='noreferrer' className="nav-link resume"><button >Resume</button></a>
+        <a href="https://docs.google.com/document/d/1_bWH28nOAnBCozL8CrT66WpHO_9jXLN_xWea7NnGeBc/edit" 
+        target="_blank" rel='noreferrer' className="nav-link resume"><button >Resume</button></a>
         <Link to="/contact" className="nav-link contact">
           <button >Contact</button>
         </Link>
       </HStack>
     </div>
+    </div>
+
   );
 };
 
